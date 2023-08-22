70e4cecec32051be1b2868a970f1838e92731a90
Gonzalo Quiroga Sassu <gonzaquirogasassu@gmail.com>
2023-08-22 09:57:28 -0300

    JavaScript Semana 1

---
+++ LABORATORIO IV/JAVASCRIPT/Semana 1/ecommerce/cliente/js/products.js
@@ -0,0 +1,16 @@
+const productos = [
+  {
+    id: 1,
+    productName: "Banana",
+    price: 480,
+    quanty: 1,
+    img: "/ecommerce/cliente/media/banana.jpg",
+  },
+  {
+    id: 2,
+    productName: "Frutilla",
+    price: 430,
+    quanty: 10,
+    img: "/ecommerce/cliente/media/frutilla.jpg",
+  },
+];
