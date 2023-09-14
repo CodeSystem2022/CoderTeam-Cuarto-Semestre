601e2ac331a577e18735873cda58156b2dcdc425
Adan Gomez <112594636+2022Adan@users.noreply.github.com>
2023-09-13 22:58:34 -0300

    Create app2.js
    Creamos Archivo App2 con contenido de la clase

---
+++ METODOLOGIA/NODE/Semana 3/HOLA-MUNDO/app2.js
@@ -0,0 +1,4 @@
+const saludar = (nombre) => { //Funcion de flecha
+    return `Saludos de ${nombre}`; //Usamos Back tic´s (Usando Alt + 96)
+}// esto es igual a : "Saludos" + nombre;
+console.log(saludar("Adan"));
