1808d95bb418c1ff1c2bc91b300a695d7f6cdbeb
Adan Gomez <112594636+2022Adan@users.noreply.github.com>
2023-10-03 09:34:14 -0300

    JS Semana 3
    JS Semana 3

--- LABORATORIO IV/JAVASCRIPT/Semana 3/cliente/js/index.js
+++ LABORATORIO IV/JAVASCRIPT/Semana 3/cliente/js/index.js
@@ -16,13 +16,24 @@ productos.forEach((product) => {
   content.append(buyButton);
 
   buyButton.addEventListener("click", ()=>{
-    cart.push({
-    id: product.id,
-    productName: product.productName,
-    price: product.price,
-    quanty: product.quanty,
-    img: product.img,
-  })
-  console.log(cart)
-})
+    const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+    if(repeat){
+      cart.map((prod)=> {
+        if(prod.id === prod.id){
+          prod.quanty++;
+        }
+      });
+    }else {
+      cart.push({
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: product.quanty,
+        img: product.img,
+        });
+        console.log(cart);
+    }
+
+  });
 });
