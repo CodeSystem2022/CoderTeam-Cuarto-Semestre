ed79309b08bbfe171e5a01acecd1a62477e03065
YolyOrtiz <112596306+yolyhil@users.noreply.github.com>
2023-10-09 12:22:35 -0300

    Create mi_clase.py

---
+++ LABORATORIO IV/PYTHON/SEMANA5/mi_clase.py
@@ -0,0 +1,21 @@
+
+class MiClase:
+    '''
+    este es un ejemplo de la documentacion de esta clase
+    esto es solo para saber que aqui esta mi clase
+    '''
+    def __init__(self):
+        '''
+        aqui esta el metodo dunder init este metodo es
+        el inicio para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el metodo creado por mi, que recibe parametros
+        Detecto los parametros automaticamente
+        :param param1: este es el parametro 1
+        :param param2: este es el parametro 2
+        :return: este es el valor de retorno
+        '''
+
