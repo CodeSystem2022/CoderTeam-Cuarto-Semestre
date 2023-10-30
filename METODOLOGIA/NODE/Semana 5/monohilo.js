6f40485469a1ff199acfbd7015ae96bea1e55293
Natan <112595785+wildBlueTurtle@users.noreply.github.com>
2023-10-29 23:40:16 -0300

    merge local file_subfolder

--- METODOLOGIA/NODE/Semana 5/monohilo.js
+++ METODOLOGIA/NODE/Semana 5/monohilo.js
@@ -1,1 +1,12 @@
+console.log('CoderTeam development to the world')
 
+var i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+/*if(i == 5){
+    console.log('Force an error');
+    var a= 3 + z;
+}*/
+}, 1000);
+console.log('second instruction')
